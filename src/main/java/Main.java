943935902731969963bee11869e8301369262bba
Moks <gutamax2@mail.ru>
2021-12-02 22:46:51 +0500

    added test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        BonusService service = new BonusService();
-
-        // подготавливаем данные:
-        long amount = 1000_60;
-        boolean registered = true;
-        long expected = 30;
-
-        // вызываем целевой метод:
-        long actual = service.calculate(amount, registered);
-
-        // производим проверку (сравниваем ожидаемый и фактический):
-        // если true - то PASS
-        // если false - то FAIL
-        boolean passed = expected == actual;
-
-        // выводим результат
-        System.out.println(passed);
+        int initialAccount = 200;
+        int replenishmentAmount = 1200;
+        int percent = 100;
+        int bonus = replenishmentAmount / percent;
+        if (replenishmentAmount > 1000) {
+            bonus = replenishmentAmount / percent;
+        } else {
+            bonus = 0;
+        }
+        int check = initialAccount + replenishmentAmount + bonus;
+        System.out.println(bonus);
+        System.out.println(check);
     }
+
 }
+
--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -49,4 +49,36 @@ class BonusServiceTest {
         // производим проверку (сравниваем ожидаемый и фактический):
         assertEquals(expected, actual);
     }
+
+    @org.junit.jupiter.api.Test
+    void shouldCalculateForExpected() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 1_000_000_60;
+        boolean registered = false;
+        long expected = 600;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldCalculateForExpectedSmaller() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 1_000_000_60;
+        boolean registered = false;
+        long expected = 1;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected, actual);
+    }
 }
